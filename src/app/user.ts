549680d8aff1deabd1b9f6972b85dceda4d277c7
Kolia <patskalWork@gmail.com>
2022-08-23 15:26:21 +0300

    Add interface User

---
+++ src/app/user.ts
@@ -0,0 +1,10 @@
+export interface User {
+    id: number;
+    name: string;
+    surname: string;
+    middlename: string;
+    sex: string;
+    status: boolean;
+    birth: Date;
+    abilities: string;
+}
