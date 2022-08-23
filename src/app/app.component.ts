4ec378b0cad4126d505fe2c0d571a1261b386ca8
Kolia <patskalWork@gmail.com>
2022-08-23 17:11:57 +0300

    Created main-page
    The structure of the main page is written in app.component.html
    
    Added page navigation buttons
    
    Added and described additional outlet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testTask';
+
+  constructor(private router: Router){}
+
+  /*
+    Хотів зробити перехід за посиланням без зміни url, але
+    не зрозумів як це зробити. Знаю, що для цього використовують 
+    
+    skipLocationChange: true.
+
+    Можливо це через метод navigate, а не navigateUrl. Поки це перевіряв.
+    Якщо десь помилився, буду радий почути коректне рішення
+  */
+
+  toTable(){
+    this.router.navigate([{ outlets: {primary: 'user-table'}}])
+  }
+
+  toForm(){
+    this.router.navigate([{ outlets: {form: 'user-form'}}])
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { UserAddFormComponent } from './components/user-add-form/user-add-form.c
 
 const routes: Routes = [
   { path: 'user-table', component: UserTableComponent },
-  { path: 'user-form', component: UserAddFormComponent },
+  { path: 'user-form', component: UserAddFormComponent, outlet: 'form' },
 ];
 
 @NgModule({
